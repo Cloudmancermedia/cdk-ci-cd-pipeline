b6780c8cfe88231c4aa21b97e741fd9976a0157f
Ryan Williams <52295078+Cloudmancermedia@users.noreply.github.com>
2024-06-18 10:34:17 -0600

    refactor to allow for provess.env in build stage

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -3,12 +3,13 @@ import { App } from 'aws-cdk-lib';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 
 const app = new App();
-const envName = app.node.tryGetContext('env')
+if (!process.env.DEPLOY_ENVIRONMENT) throw new Error("DEPLOY_ENVIRONMENT is not defined.")
+const { DEPLOY_ENVIRONMENT } = process.env;
 new InfrastructureStack(
   app,
-  `${envName}-Infrastructure-Stack`, 
+  `${DEPLOY_ENVIRONMENT}-Infrastructure-Stack`, 
   {
-    envName,
-    description: `Stack for the ${envName} infrastructure deployed using the CI pipeline. If you need to delete everything involving the ${envName} environment, delete this stack first, then the CI stack.`
+    DEPLOY_ENVIRONMENT,
+    description: `Stack for the ${DEPLOY_ENVIRONMENT} infrastructure deployed using the CI pipeline. If you need to delete everything involving the ${DEPLOY_ENVIRONMENT} environment, delete this stack first, then the CI stack.`
   }
 );
